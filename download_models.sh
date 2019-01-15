aa37081938f541942175a82a78cabf5013ae285c
madhawav <madhawavidanapathirana@gmail.com>
2019-01-15 16:03:22 +0530

    Modify download_models.sh to enable testing mode of .cfg after downloading

--- download_models.sh
+++ download_models.sh
@@ -6,6 +6,12 @@ mkdir cfg
 wget -O cfg/coco.data https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/coco.data
 wget -O cfg/yolov3.cfg https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
 
+echo "Modify config parameters to enable Testing mode"
+sed -i '/batch=64/c\# batch=64' cfg/yolov3.cfg
+sed -i '/subdivisions=16/c\# subdivisions=16' cfg/yolov3.cfg
+sed -i '/# batch=1/c\batch=1' cfg/yolov3.cfg
+sed -i '/# subdivisions=1/c\subdivisions=1' cfg/yolov3.cfg
+
 mkdir data
 wget -O data/coco.names https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
 
